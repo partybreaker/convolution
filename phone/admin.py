c3636a497c0adb8154560cac41b0c9b2af28d499
partybreaker <k.tio@donnu.edu.ua>
2018-12-07 02:32:21 +0200

    Fix админки

--- phone/admin.py
+++ phone/admin.py
@@ -4,35 +4,54 @@ from .models import Cam, CPU, ROM, Manufacture, Screen, Coefficient, Battary, Ph
 
 # Register your models here.
 class CamAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_cam_coeff',)
 
+
+    def get_cam_coeff(self, object):
+        return object.coefficient
+    get_cam_coeff.short_description = 'Коэффициент'
 
 admin.site.register(Cam, CamAdmin)
 
 
 class CPUAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_cpu_coeff', )
+
+    def get_cpu_coeff(self, object):
+        return object.coefficient
+    get_cpu_coeff.short_description = 'Коэффициент'
 
 
 admin.site.register(CPU, CPUAdmin)
 
 
 class ROMAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_rom_coeff',)
+
+    def get_rom_coeff(self, object):
+        return object.coefficient
+    get_rom_coeff.short_description = 'Коэффициент'
 
 
 admin.site.register(ROM, ROMAdmin)
 
 
 class ManufactureAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_manf_coeff',)
 
+    def get_manf_coeff(self,object):
+        return object.coefficient
+    get_manf_coeff.short_description = 'Коэффициент'
 
 admin.site.register(Manufacture, ManufactureAdmin)
 
 
 class ScreenAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'get_screen_coeff',)
+
+    def get_screen_coeff(self, object):
+        return object.coefficient
+    get_screen_coeff.short_description = 'Коэффициент'
 
 
 admin.site.register(Screen, ScreenAdmin)
@@ -46,8 +65,11 @@ admin.site.register(Coefficient, CoefficientAdmin)
 
 
 class BattaryAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name', 'get_batt_coeff', )
 
+    def get_batt_coeff(self, object):
+        return object.coefficient
+    get_batt_coeff.short_description = 'Коэффициент'
 
 admin.site.register(Battary, BattaryAdmin)
 
@@ -55,7 +77,10 @@ admin.site.register(Phone)
 
 
 class PriceAdmin(admin.ModelAdmin):
-    list_display = ('value',)
+    list_display = ('value', 'get_price_coeff',)
 
+    def get_price_coeff(self, object):
+        return object.coefficient
+    get_price_coeff.short_description = 'Коэффициент'
 
 admin.site.register(Price, PriceAdmin)
